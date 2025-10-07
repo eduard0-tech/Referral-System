041b0ed6d28b6089a2387216f9e42296f8c7dc50
Luis Eduardo <luiseduardocbft@gmail.com>
2025-10-07 19:49:37 -0300

    Fix: Usa variável de ambiente para criar link de indicação

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -1,6 +1,6 @@
-const User = require('../models/User'); 
+const User = require('../models/User');
 
-// Registrar um novo usuário
+// @desc    Registrar um novo usuário
 const registerUser = async (req, res) => {
     const { name, email, password, referralCode } = req.body;
 
@@ -11,31 +11,24 @@ const registerUser = async (req, res) => {
             return res.status(400).json({ message: 'Este e-mail já está cadastrado.' });
         }
 
-        const user = new User({
-            name,
-            email,
-            password,
-        });
+        const user = new User({ name, email, password });
 
         if (referralCode) {
-            // Encontra o usuário que possui esse código de indicação
             const referrer = await User.findOne({ referralCode: referralCode });
-
             if (referrer) {
-                // Se encontrou, adiciona 1 ponto a ele
                 referrer.points += 1;
-                await referrer.save(); // Salva a alteração no usuário indicador
-                console.log(`Ponto adicionado ao usuário: ${referrer.name}`);
+                await referrer.save();
             }
         }
 
         const createdUser = await user.save();
+
         res.status(201).json({
             _id: createdUser._id,
             name: createdUser.name,
             email: createdUser.email,
             points: createdUser.points,
-            referralLink: `http://localhost:5173/?ref=${createdUser.referralCode}` 
+            referralLink: `${process.env.FRONTEND_URL}/?ref=${createdUser.referralCode}`
         });
 
     } catch (error) {
@@ -43,26 +36,22 @@ const registerUser = async (req, res) => {
     }
 };
 
-// Autenticar (logar) um usuário
+// @desc    Autenticar (logar) um usuário
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        // Encontra o usuário pelo email
         const user = await User.findOne({ email });
 
-        // Se o usuário existir E a senha digitada bater com a senha criptografada no banco...
         if (user && (await user.matchPassword(password))) {
-            // Envia os dados do usuário para o front-end
             res.json({
                 _id: user._id,
                 name: user.name,
                 email: user.email,
                 points: user.points,
-                referralLink: `http://localhost:5173/?ref=${user.referralCode}`
+                referralLink: `${process.env.FRONTEND_URL}/?ref=${user.referralCode}`
             });
         } else {
-            // Se não, envia um erro de credenciais inválidas
             res.status(401).json({ message: 'E-mail ou senha inválidos.' });
         }
     } catch (error) {
@@ -70,11 +59,11 @@ const loginUser = async (req, res) => {
     }
 };
 
-// Pegar o perfil do usuário
+// @desc    Pegar o perfil do usuário
+// @route   GET /api/user/:id
 const getUserProfile = async (req, res) => {
     try {
-        // Encontra o usuário pelo ID que veio na URL (:id)
-        const user = await User.findById(req.params.id).select('-password'); // .select('-password') remove a senha da resposta
+        const user = await User.findById(req.params.id).select('-password');
 
         if (user) {
             res.json({
